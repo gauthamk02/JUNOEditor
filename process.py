f1e5eefca0857058249bd28e1d2484f8e6a89364
R.V.Rajagopalan <rajagopalan602@gmail.com>
2022-10-01 18:47:23 +0530

    process updated

--- process.py
+++ process.py
@@ -6,41 +6,52 @@ import cv2, imutils
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.setFixedSize(536, 571)
+        MainWindow.setFixedSize(800, 800)
 
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+
         self.gridLayout_2 = QtWidgets.QGridLayout(self.centralwidget)
         self.gridLayout_2.setObjectName("gridLayout_2")
+
         self.gridLayout = QtWidgets.QGridLayout()
         self.gridLayout.setObjectName("gridLayout")
-        self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
-        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setText("")
-        #self.label.setPixmap(QtGui.QPixmap("images/2.jpg"))
         self.label.setObjectName("label")
-        self.horizontalLayout_3.addWidget(self.label)
-        self.horizontalLayout = QtWidgets.QHBoxLayout()
+
+        
+        self.horizontalLayout = QtWidgets.QVBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
+
         self.verticalSlider = QtWidgets.QSlider(self.centralwidget)
         self.verticalSlider.setOrientation(QtCore.Qt.Horizontal)
         self.verticalSlider.setObjectName("verticalSlider")
         self.horizontalLayout.addWidget(self.verticalSlider)
+
         self.verticalSlider_2 = QtWidgets.QSlider(self.centralwidget)
         self.verticalSlider_2.setOrientation(QtCore.Qt.Horizontal)
         self.verticalSlider_2.setObjectName("verticalSlider_2")
         self.horizontalLayout.addWidget(self.verticalSlider_2)
+
+        self.horizontalLayout_3 = QtWidgets.QVBoxLayout()
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        self.horizontalLayout_3.addWidget(self.label)
         self.horizontalLayout_3.addLayout(self.horizontalLayout)
         self.gridLayout.addLayout(self.horizontalLayout_3, 0, 0, 1, 2)
         self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+
+        # open button
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_2.setObjectName("pushButton_2")
         self.horizontalLayout_2.addWidget(self.pushButton_2)
+
+        # save button
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setObjectName("pushButton")
         self.horizontalLayout_2.addWidget(self.pushButton)
+
         self.gridLayout.addLayout(self.horizontalLayout_2, 1, 0, 1, 1)
         spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.gridLayout.addItem(spacerItem, 1, 1, 1, 1)
@@ -55,6 +66,7 @@ class Ui_MainWindow(object):
         self.verticalSlider_2.valueChanged['int'].connect(self.blur_value)
         self.pushButton_2.clicked.connect(self.loadImage)
         self.pushButton.clicked.connect(self.savePhoto)
+        
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
         
         # Added code here
